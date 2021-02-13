2a9d66f3fba91077e206c5285543441eaaf5f166
Gilmart Quentin <quentin.gilmart@student.vinci.be>
2021-02-13 09:40:58 +0100

    :wrench: Change data

--- src/data.js
+++ src/data.js
@@ -5,11 +5,11 @@ export const data = {
       answer: 'cb76796a8b1691ff1184a2c85b40787665d82e85',
     },
     {
-      question: 'Peut-être trouveras-tu un deuxième code à l\'endroit où nous nous sommes embrassés la première fois',
+      question: 'Peut-être trouveras-tu un deuxième code à l\'endroit où nous nous sommes embrassés la première fois ?',
       answer: 'c1b631742b8675250aaf14bd9e7fd4db9b8b7a04',
     },
     {
-      question: 'Il faisait froid ce jour là, rendez-vous là où nous avons pris à manger lorsd de notre première rencontre',
+      question: 'Le troisième code est derrière l\'une des soeurs de ce que j\'ai "emprunté" pour toi',
       answer: 'fa925d5136fef657574c8af0bedf39234de69ebf',
     },
     {
@@ -17,7 +17,7 @@ export const data = {
       answer: 'b652099d4e5916c53df409b575a7f37b39c7c591',
     },
     {
-      question: 'Félicitations mon amour, tu es la plus forte !!',
+      question: 'Félicitations mon amour, tu es la plus forte !! Ouvre ton cadeau...',
     }
   ]
 }
